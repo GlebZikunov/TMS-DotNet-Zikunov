7a61cb3d5df3c41a1a2b3ab7f8ea72f6e1941f19
Gleb <79657936+GlebZikunov@users.noreply.github.com>
2021-09-23 22:24:09 +0300

    feat: add solution of homework 1
    feat: add solution of homework 1

---
+++ src/TeachMeSkills.Zikunov.Homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills.Zikunov.Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Gleb!");
+        }
+    }
+}
