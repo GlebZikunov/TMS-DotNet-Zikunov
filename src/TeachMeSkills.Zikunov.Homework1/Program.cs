af9e74a88a1e4c2c8d0e28e08b1c359627b8f8de
Gleb Zikunov <zikunovga@gmail.com>
2021-09-27 17:37:53 +0300

    Update Program.cs
    feat: add solution of homework 1

--- src/TeachMeSkills.Zikunov.Homework1/Program.cs
+++ src/TeachMeSkills.Zikunov.Homework1/Program.cs
@@ -4,9 +4,32 @@ namespace TeachMeSkills.Zikunov.Homework1
 {
     class Program
     {
+        public static int numberOfTheDay(int DD, int MM, int YY)
+        {
+            int a, y, m, R;
+            a = (14 - MM) / 12;
+            y = YY - a;
+            m = MM + 12 * a - 2;
+            R = 7000 + (DD + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12);
+
+            return R % 7;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Gleb!");
+            string[] daysOfWeek = { "Sunday", "Monday", "Tuersday",
+            "Wednesday", "Thirsday", "Friday", "Saturday" };
+
+            Console.WriteLine("Enter day: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter month: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\n\nDay of the week is {daysOfWeek[numberOfTheDay(day, month, year)]}");
+            Console.ReadKey();
         }
     }
 }
