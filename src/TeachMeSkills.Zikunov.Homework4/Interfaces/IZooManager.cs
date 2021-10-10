ca1c53ad0e0914da1cdf4a5637f22d0a3a075723
Gleb Zikunov <zikunovga@gmail.com>
2021-10-10 19:07:42 +0300

    feat: add solution of homework 4

---
+++ src/TeachMeSkills.Zikunov.Homework4/Interfaces/IZooManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachMeSkills.Zikunov.Homework4.Interfaces
+{
+    interface IZooManager
+    {
+        List<Models.AnimalBase<int>> Animals { get; set; }
+
+        void Show();
+    }
+}
